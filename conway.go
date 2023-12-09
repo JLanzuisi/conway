116a45577a2995778496c9fbd2fad42fe6a2f08d
Jhonny Lanzuisi <jalb97@gmail.com>
2023-12-09 13:22:58 -0400

    Add main file

--- conway.go
+++ conway.go
@@ -1,6 +1,5 @@
 package main
 
-import "fmt"
 import "maps"
 
 type Cell [2]int
@@ -78,19 +77,4 @@ func NextGen(grid Grid) (nextgen Grid) {
 	}
 
 	return nextgen
-}
-
-func main() {
-	cells := CellSet{
-		Cell{1, 2}: true,
-		Cell{0, 2}: true,
-		Cell{2, 2}: true,
-	}
-
-	grid := Grid{cells, [2]int{10, 10}}
-
-	nextgen := NextGen(grid)
-
-	fmt.Printf("Before: %+v\n", grid.cells)
-	fmt.Printf("After: %+v\n", nextgen.cells)
 }
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	cells := CellSet{
+		Cell{1, 2}: true,
+		Cell{0, 2}: true,
+		Cell{2, 2}: true,
+	}
+
+	grid := Grid{cells, [2]int{10, 10}}
+
+	nextgen := NextGen(grid)
+
+	fmt.Printf("Before: %+v\n", grid.cells)
+	fmt.Printf("After: %+v\n", nextgen.cells)
+}
